e35ae5d23e5628acb61c83f5f039a7eb920b9b85
pzhihao <864300472@qq.com>
2017-01-13 15:32:17 +0800

    jsp页面展示值

--- src/com/ailtt/action/loveAction.java
+++ src/com/ailtt/action/loveAction.java
@@ -2,16 +2,23 @@ package com.ailtt.action;
 
 import com.ailtt.bean.Book;
 import com.ailtt.service.BookService;
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.interceptor.RequestAware;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
  * Created by Administrator on 2017/1/13.
  */
-public class loveAction extends ActionSupport {
+public class loveAction extends ActionSupport implements RequestAware{
 
     @Autowired
     BookService bookService;
@@ -30,8 +37,15 @@ public class loveAction extends ActionSupport {
             t.info("信息："+book.getName());
         }
 
+        HttpServletRequest request = ServletActionContext.getRequest();
+        request.setAttribute("list",allBook);
 
 
         return "success";
+    }
+
+    @Override
+    public void setRequest(Map<String, Object> map) {
+
     }
 }
--- web/WEB-INF/views/love.jsp
+++ web/WEB-INF/views/love.jsp
@@ -1,3 +1,5 @@
+<%@ taglib prefix="s" uri="/struts-tags" %>
+
 <%--
   Created by IntelliJ IDEA.
   User: Administrator
@@ -11,6 +13,19 @@
     <title>Title</title>
 </head>
 <body>
-    <span style="font-size: 32px;color: red">爱你么么哒</span>
+
+    <s:iterator value="#request.list" var="o" status="st">
+        <s:if test="#st.odd">
+            <s:if test="#st.index == 0">
+                <span style="font-size: 32px;color: red"><s:property value="#o.name"></s:property></span><br>
+            </s:if>
+            <s:else>
+                <span style="font-size: 32px;color: aqua"><s:property value="#o.name"></s:property></span><br>
+            </s:else>
+        </s:if>
+        <s:else>
+            <span style="font-size: 32px;color: green"><s:property value="#o.name"></s:property></span><br>
+        </s:else>
+    </s:iterator>
 </body>
 </html>
